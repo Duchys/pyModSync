f7cb1662d9878529fdfe7ce5e8eafda8dc239a1a
Duchys <theduchys@gmail.com>
2020-12-19 01:09:45 +0100

    FileDownloader was transformed to fuction

--- FileDownloader.py
+++ FileDownloader.py
@@ -1,25 +1,31 @@
-import requests
+def FileDownloader(file_url,file_destination_path):
+    import requests
+#TODO: find an alternative which works with GUI aswell
+    from clint.textui import progress
 #TODO: print which file is being downloaded
 #Print Addon>filename is being downloaded 
-print('Starting the download of @ace3/authors.txt')
-
-url = 'https://a3.417rct.org/addons/%40ace3/authors.txt'\
+    print('Starting the download of @ace3/authors.txt')
 #TODO: Add configuration option which allows insecure downloads (incase self signed certs are user [or addon repository has expired certificate])
-r = requests.get(url)
+#TODO: Rename this variable
+    file_stream = requests.get(file_url, stream=True)
+    print(file_stream.status_code)
+#TODO: Set this based on the repository output
+#Sets the path to which the file should be put
 #TODO: Allow user to specifiy the path
 #TODO: Create the @Addon directory (eg.@ace3) (the directory will be granted by the xml with list of addons)
 #TODO: Create the Subdiretory in @Addon (eg. @ace3/assets) (the subdirectory name will be granted by the xml with list of addons)
 #TODO: Name the file in the same way as specified in the xml list of addons
 #TODO: Check if forward slashes work on Windows aswell
 #TODO: Optimize the download to not always download the entire file but just the changed parts if possible 
+#TODO: Allow pause of the download https://stackoverflow.com/questions/12243997/how-to-pause-and-resume-download-work
 #Write the file to prespecified path by user
-with open('/tmp/authors.txt', 'wb') as f:
-    f.write(r.content)
 
-# Retrieve HTTP meta-data
-#TODO: move this as it is unnecessary here (maybe leave status code for some initial check of the repository, this however will be done in when the repository will be downloaded)
-print(r.status_code)
-#TODO: delete this
-print(r.headers['content-type'])
-#TODO: move this as it is unnecessary here (maybe leave status code for some initial check of the repository, this however will be done in when the repository will be downloaded)
-print(r.encoding)
+
+    with open(file_destination_path, 'wb') as downloaded_file:
+        total_length = int(file_stream.headers.get('content-length'))
+        for chunk in progress.bar(file_stream.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+            if chunk:
+                downloaded_file.write
+                downloaded_file.flush
+
+    print('File Downloaded successfuly')
