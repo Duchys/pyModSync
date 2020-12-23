221dfe3bd5f09c0a115051b83b57ea5617dd0715
Duchys <Duchysekop@gmail.com>
2020-12-23 21:36:17 +0100

    Commets added

--- src/CheckForUpdate.py
+++ src/CheckForUpdate.py
@@ -1,21 +1,25 @@
 
 
 #TODO: Add comments
+#Repository comparison
 def compare_repositories(local_repository, remote_repository_destination_path, repository_difference_outfile):
+        #Opens repo csv files
         with open(local_repository, 'r') as t1, open(remote_repository_destination_path, 'r') as t2:
-            file_one = t1.readlines()
-            file_two = t2.readlines()[2:]
+            local_repo = t1.readlines()
+            #skips first 2 lines
+            remote_repo = t2.readlines()[2:]
+        #Compares local repository with remote repostitory
+        with open(repository_difference_outfile, 'w') as repo_diff:
+            #Each line present in remote repostiory gets compared to the local_repository. If the line is not present (ie. not exact match) it gets put to a repo_diff .csv file.
+            for line in remote_repo:
+                if line not in local_repo:
+                    repo_diff.write(line)
 
-        with open(repository_difference_outfile, 'w') as out_file:
-            for line in file_two:
-                if line not in file_one:
-                    out_file.write(line)
-
+#Checks if there is any difference between repositories based on output of compare_repositories
 def check_for_update(local_repository, remote_repository_destination_path, repository_difference_outfile):
     compare_repositories(local_repository, remote_repository_destination_path, repository_difference_outfile)
     import os
     if not os.stat(repository_difference_outfile).st_size == 0:
         print ('Change between local and remote repository was detected')
     else:
-        
         print ('No change between local and remote repository was detected')
