99a323526ecbdb444454a485f45368d978477a53
Duchys <Duchysekop@gmail.com>
2020-12-26 01:39:57 +0100

    removed debug line

--- src/ServerSide/ServerChecksumGenerator.py
+++ src/ServerSide/ServerChecksumGenerator.py
@@ -54,7 +54,6 @@ def repository_generator(repository_name, path_to_modlist, source_addon_director
             if path_processed.startswith('/'):
                 path_processed = path_processed[1:]
                 #If path modified for validation starts with any of strings provided by the modpack.ini file then craete a checksum for it and write it to repository.csv
-            print(path_processed)
             if path_processed.startswith(tuple(modlist_processed)):
                     #Removes / from the path incase the path did not end with a /
                 writer.writerow((path_processed, file_hash_hex(path, hashlib.blake2b)))
