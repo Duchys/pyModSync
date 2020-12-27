0eec45a2b481c4449057ac5ebb9b8c8fb00dace0
Duchys <Duchysekop@gmail.com>
2020-12-27 01:10:19 +0100

    Comments, removal of RepositoryLoader.py

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,4 @@
-from RepositoryLoader import repository_downloader
+from FileDownloader import file_downloader
 from CheckForUpdate import compare_repositories
 from CheckForUpdate import check_for_update
 from UserChecksumGenerator import local_repository_generator
@@ -7,10 +7,10 @@ import os
 #TODO: Check if forward slashes work on Windows aswell @Furi
 remote_repository_url = 'https://a3.417rct.org/addons/a_debilek_roku_vyhrava_duchy.csv'
 remote_repository_destination_path = '/home/duchys/Documents/remoterepository.csv'
-#Download repository
 print('...........................................')
 print ('Updating the remote repository')
-repository_downloader(remote_repository_url, remote_repository_destination_path)
+#Download repository
+file_downloader(remote_repository_url, remote_repository_destination_path)
 
 #Compare changed files between local addon repostiory (eg. Franta Cihla's local mod folder [/mnt/addons]) and remote addon repository (eg. 417RCT Official Repository)
 local_repository= '/home/duchys/Documents/localrepo.csv'
@@ -19,9 +19,12 @@ repository_difference_outfile= '/home/duchys/Documents/repodiffoutfile.csv'
 
 print('Checking if local repository already exists')
 print('...........................................')
+#Checks if file local repository already exists, if not it calls the generator to generate the local repository
 if not os.path.isfile(local_repository):
     print('local repository was not found, generating local repository')
     local_repository_generator(local_addon_path, local_repository)
+#Checks if the local repository file is empty, if so, regenerates the local repository
+#this is done in order to prevent redownload of already downloaded files incase some issue arose during the generation of the initial local repository 
 elif os.stat(local_repository).st_size == 0:
     print('File exists and is empty, regenerating local repository')
     local_repository_generator(local_addon_path, local_repository)
@@ -31,5 +34,7 @@ print('...........................................')
 
 
 print('Checking for differences between local and remote repository')
+#Compare repositories for any differences, and write them to an outfile
 compare_repositories(local_repository, remote_repository_destination_path, repository_difference_outfile)
+#Check if outfile for repository difference contains any data
 check_for_update(local_repository, remote_repository_destination_path, repository_difference_outfile)
