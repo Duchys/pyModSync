e4e3e8c3c92bac9afd60d21c20d9fd756db0b710
Duchys <Duchysekop@gmail.com>
2020-12-27 01:33:17 +0100

    Deletion of useless file

--- src/FileChangeDetector.py
+++
@@ -1,8 +0,0 @@
-#GOAL: The point of this file is to compare the local files with the files provided by the repository
-#After a change is found provide it to the FileDownloader which will download then download the file
-#If a file is missing, provide the link to the file to the FileDownloader
-
-#Possible checks:
-#checksum (provide checksum generator for repo)
-#TODO: Compare checksums
-#TODO: Make this a function to be used to compare downloaded repository
