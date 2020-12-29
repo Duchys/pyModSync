debf76a09c5b69877ac9dfe262389c1c40a7cf33
Duchys <Duchysekop@gmail.com>
2020-12-29 04:28:30 +0100

    crash prevention incase remote repository is unavailable

--- src/Main.py
+++ src/Main.py
@@ -6,15 +6,18 @@ from FileUpdateRequester import file_update_requester
 import os
 #TODO: Allow user to specifiy the path
 #TODO: Check if forward slashes work on Windows aswell @Furi
-remote_repository_url = 'https://a3.417rct.org/addons/a_debilek_roku_vyhrava_duchy.csv'
+remote_repository_url = 'https://a3.417rct.org/addons/a_debilek_roku_vyhravaz_duchy.csv'
 if remote_repository_url.endswith('/'):
     remote_repository_url = remote_repository_url[:-1]
 remote_repository_destination_path = '/home/duchys/Documents/remoterepository.csv'
 print('...........................................')
 print ('Updating the remote repository')
 #Download repository
-file_downloader(remote_repository_url, remote_repository_destination_path)
-
+#Try and except is used to prevent crash incase the remote repository is unavailable
+try:
+    file_downloader(remote_repository_url, remote_repository_destination_path)
+except:
+    print('failed to download the repository, cached repository will be used...')
 #Compare changed files between local addon repostiory (eg. Franta Cihla's local mod folder [/mnt/addons]) and remote addon repository (eg. 417RCT Official Repository)
 local_repository= '/home/duchys/Documents/localrepo.csv'
 
