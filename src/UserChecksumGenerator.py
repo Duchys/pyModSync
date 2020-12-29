e9451e664ac82cc1b7bb16d4d69df2fa30a90835
Duchys <Duchysekop@gmail.com>
2020-12-29 03:58:05 +0100

    "Simplification"

--- src/UserChecksumGenerator.py
+++ src/UserChecksumGenerator.py
@@ -16,29 +16,34 @@
 #@TFAR/modfilea.pbo checksum
 #@AGM/modfilea.pbo checksum
 #@AGM/modfilea.pbo checksum
-import sys
+
 #Based on this output addons @ACE, @TFAR, @AGM will be enabled.
-#TODO: generate the 
+#TODO: generate the
+import sys
+import csv
+import hashlib
+import os
+import shutil
+
+def file_hash_hex(file_path, hash_func):
+    with open(file_path, 'rb') as f:
+        return hash_func(f.read()).hexdigest()
+
+def sorted_file_listing(base_dir):
+    for directory, subdirs, files in sorted(os.walk(base_dir)):
+        for filename in files:
+            yield os.path.join(directory, filename)
+
 def local_repository_generator(source_addon_directory, checksum_output_destination):
-    import csv
-    import hashlib
-    import os
-    import shutil
 #TODO: Get someone who knows what he is doing, and not just doing CTRL + C from the web until it starts working...
 
-    def file_hash_hex(file_path, hash_func):
-        with open(file_path, 'rb') as f:
-            return hash_func(f.read()).hexdigest()
-    def sorted_file_listing(base_dir):
-        for directory, subdirs, files in sorted(os.walk(base_dir)):
-            for filename in files:
-                yield os.path.join(directory, filename)
+
+
 #TODO: add comments so it seems like i know what I'm doing
     checksum_output_temp_destination=checksum_output_destination+'_temp'
-    with open(checksum_output_temp_destination, 'w', newline='',encoding='utf-8') as f:
+    with open(checksum_output_temp_destination, 'w', newline='',encoding='utf-8') as outlocalrepo:
         print('this will take some time, go and grab a coffee')
-        writer = csv.writer(f)
-        writer = csv.writer(f, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(outlocalrepo, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for path in sorted_file_listing(source_addon_directory):
             path_processed=path.replace(source_addon_directory,'')
             #Removes / from the path incase the path did not end with a /
