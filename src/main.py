01e5e40ad9e029fe5889b9082a316e9d8a4884ab
Duchys <Duchysekop@gmail.com>
2021-03-13 15:19:40 +0100

    short description of functions

--- src/main.py
+++ src/main.py
@@ -7,5 +7,9 @@ from update_addons import update_addons
 # download changed addons
 # Compare changed files between local addon repostiory (eg. Franta Cihla's local mod folder [/mnt/addons])
 # and remote mod repository (eg. 417RCT Official Repository)
+
+# Check if config file exists, if not create it, then create local addon repository
+# After that check for differences between local and remote repository
 check_addons()
+# Update out of date addons based on the output of check_addons function
 update_addons()
