2532b61a6625bc5869396798ccd1a5561b8d1160
Duchys <Duchysekop@gmail.com>
2021-03-27 23:23:23 +0100

    Added comments + logging

--- src/check_addons.py
+++ src/check_addons.py
@@ -4,6 +4,7 @@ from repository_downloader import repository_downloader
 from local_repository_manager import check_for_local_repository
 from update_manager import compare_repositories
 from update_manager import check_for_update
+from logger import logger
 
 
 # File intended to be called by GUI
@@ -20,18 +21,24 @@ def check_addons():
     Compare changed files between local addon repostiory (eg. Franta Cihla's local mod folder [/mnt/addons])
     and remote mod repository (eg. 417RCT Official Repository)
     """
+    # Check if config file exists, if not create it
     check_if_config_exists()
-    print('loading the config...')
+    # Define log config by calling logger function
+    log = logger()
+    log.info('Loading configuration')
+    print('Loading configuration')
+    # Load configuration
     config = config_loader()
     remote_repository_url = config[0]
     local_repository = config[1]
     repository_difference_outfile = config[2]
     remote_repository_destination_path = config[3]
     local_addon_path = config[4]
-
+    # Download repository
     repository_downloader(remote_repository_url, remote_repository_destination_path)
+    # Check for differences
     check_for_local_repository(local_addon_path, local_repository)
-
+    log.info('Checking for differences between local and remote repository')
     print('Checking for differences between local and remote repository')
     print('...........................................')
     # Compare repositories for any differences, and write them to an outfile
